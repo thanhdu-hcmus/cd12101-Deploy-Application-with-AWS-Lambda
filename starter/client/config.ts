2fbc48b84cb305dc49c1e400d86355f4428be4cf
thanhdu <thanhdu-hcmus@gmail.com>
2024-12-23 20:29:07 +0700

    Add a client source code for this project

---
+++ starter/client/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    apiId: 'https://2qrhwe2t0c.execute-api.us-east-1.amazonaws.com/dev',  // Your API ID from AWS API Gateway
+    auth0: {
+      domain: 'dev-l3284qzawfkabigw.us.auth0.com',  // Your Auth0 domain
+      clientId: 'my8oncQOZVCVzXiHiG67XdFi2P9ZEYeD',    // Your Auth0 client ID
+    },
+    // Any other configuration settings needed for your frontend
+  };
